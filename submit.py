b3d386c5a57b4c5fe95ffee92c0fcee785a75603
JoniKarboni <44379942+JoniKarboni@users.noreply.github.com>
2018-10-23 08:22:15 +0200

    Update submit.py

--- submit.py
+++ submit.py
@@ -1,10 +1,10 @@
 class Trie():
     def __init__ (self):
-        self.children = {None:None} # dictionary with key = character and value = Node object of next node
-									# root 	>> self.children = {0: Node object at 0x7fd672cb02b0}
-                                    # 0 	>> self.children = {1: Node object at 0x7fd672cb0588}
-                                    # 1 	>> self.children = {2: Node object at 0x7fd672cb05c0}
-                                    # 2 	>> self.children = {3: Node object at 0x7fd672cb0630}
+        self.children = {None:None} 	# dictionary with key = character and value = Node object of next node
+					# root 	>> self.children = {0: Node object at 0x7fd672cb02b0}
+                                    	# 0 	>> self.children = {1: Node object at 0x7fd672cb0588}
+                                    	# 1 	>> self.children = {2: Node object at 0x7fd672cb05c0}
+                                    	# 2 	>> self.children = {3: Node object at 0x7fd672cb0630}
     
     # check if the character already exists in Trie. 
     # If not, the character will be added to the Trie as child of root or child of a character which already is in Trie
